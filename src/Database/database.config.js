399ca518c8233f290b7ce4db312b75cf4b4e132a
shahzaib <ishahzaibaravian@gmail.com>
2024-08-05 21:01:52 +0500

    database connection keep alive fix

--- src/Database/database.config.js
+++ src/Database/database.config.js
@@ -3,7 +3,8 @@ const connection = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
+    enableKeepAlive : true
 });
 
 connection.connect((err)=>{
